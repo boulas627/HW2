74969f71ce98ceafd172bc390bd5eded68a3419c
boulas627 <43757451+boulas627@users.noreply.github.com>
2018-11-24 17:00:49 -0500

    Add files via upload

---
+++ HW2.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$("#default").click(function() {
+		alert("Medium Level Of Difficulty"); 
+	});
+
+	$("#easy").click(function() {
+		alert("Easiest Level Of Difficulty"); 
+	});
+
+	$("#hard").click(function() {
+		alert("Easiest Level Of Difficulty"); 
+	});
+});
