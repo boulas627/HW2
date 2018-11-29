9e1ec713067b9e285fc4dfa1a4340fbeb39b4cb6
Justin Gaskins <gaskij@rpi.edu>
2018-11-29 14:43:41 -0500

    Added JQuery Slider with randomized starting values

--- HW2.js
+++ HW2.js
@@ -1,24 +1,54 @@
-$(document).ready(function() {
-//	$("#default").click(function() {
-//		alert("Medium Level Of Difficulty"); 
-//	});
-//
-//	$("#easy").click(function() {
-//		alert("Easiest Level Of Difficulty"); 
-//	});
-//
-//	$("#hard").click(function() {
-//		alert("Easiest Level Of Difficulty"); 
-//	});
-    
-    var color = getRandomColor();
-    var swatchcont = document.getElementById("swatchcont");
-    var swatch = '<div id="swatch" style="width:100px;height:100px;background-color:' + color + ';" ></div>';
-    
-    swatchcont.innerHTML = '<div>' + color + '</div>' + swatch;
+$(function() {
 
-//    swatchcont.style = 'width:100px;height:100px;background-color:' + color + ';';
+    var r = getRandomColor255();
+    var g = getRandomColor255();
+    var b = getRandomColor255();
+    var swatchcont = document.getElementById("swatchcont");
+//    var swatch = '<div id="swatch" style="width:100px;height:100px;background-color:' + color + ';" ></div>';
     
+    swatchcont.innerHTML = '<div>#' + hexFromRGB(r, g, b) + '</div>';
+
+    function hexFromRGB(r, g, b) {
+      var hex = [
+        r.toString( 16 ),
+        g.toString( 16 ),
+        b.toString( 16 )
+      ];
+      $.each( hex, function( nr, val ) {
+        if ( val.length === 1 ) {
+          hex[ nr ] = "0" + val;
+        }
+      });
+      return hex.join( "" ).toUpperCase();
+    }
+    function refreshSwatch() {
+      var red = $( "#red" ).slider( "value" ),
+        green = $( "#green" ).slider( "value" ),
+        blue = $( "#blue" ).slider( "value" ),
+        hex = hexFromRGB( red, green, blue );
+      $( "#swatch" ).css( "background-color", "#" + hex );
+    }
+ 
+    $( "#red, #green, #blue" ).slider({
+      orientation: "horizontal",
+      range: "min",
+      max: 255,
+      value: 127,
+      slide: refreshSwatch,
+      change: refreshSwatch
+    });
+    $( "#red" ).slider( "value", r );
+    $( "#green" ).slider( "value", g );
+    $( "#blue" ).slider( "value", b );
+        
+//    (function ( $ ) {
+//        $.fn.hexed = function() {
+//            var color = getRandomColor();
+//            var swatchcont = document.getElementById("swatchcont");
+//            var swatch = '<div id="swatch" style="width:100px;height:100px;background-color:' + color + ';" ></div>';
+//            swatchcont.innerHTML = '<div>' + color + '</div>' + swatch;
+//        };
+//    }( jQuery ));
 });
 
 // random color generator 
@@ -37,3 +67,6 @@ function getRandomColor() {
 	return color; 
 }
 
+function getRandomColor255() {
+	return Math.floor((Math.random() * 255) + 1);
+}
